85570950584ad670bae82adf7ae0f0042ac53401
Jhonatan Alexis Ossa Osorio <jhoalex2525@hotmail.com>
2021-08-25 08:50:54 -0500

    se crean referencias del dom

--- controlador.js
+++ controlador.js
@@ -1,1 +1,13 @@
 console.log("Hola mundo");
+
+let boton = document.getElementById("boton");
+let foto1 = document.getElementById("foto1");
+let titulo1 = document.getElementById("titulo1");
+let audio1 = document.getElementById("audio1");
+boton.addEventListener("click",perroGurdian);
+
+function perroGurdian (){
+    titulo1.textContent = "Europes Skies";
+    foto1.src = "img/foto2.jpg";
+    audio1.src = "audio/Alexander Rybak 13 horses.mp3";
+}
